22bfe59280dce0ef4872a1610b823a33abb2ad78
Laurie <laurie.beduneau@gmail.com>
2021-11-03 09:52:52 +0100

    continue working on lightbox

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -3,12 +3,13 @@ export class Lightbox {
     /**
      * 
      * @param {string} url URL de l'image
+     * @param {string[]} tab tab with all media of photographer
      */
-    constructor (tab, url) {
+    constructor (tab, url, id) {
         this._tab = tab;
         this._url = url;
-        console.log(url);
-        this.element = this.buildDom(url);
+        this._id = id;
+        this.element = this.buildDom(url, id);
         this.onKeyUp = this.onKeyUp.bind(this);
         document.body.appendChild(this.element);
         document.addEventListener('keyup', this.onKeyUp);
@@ -25,7 +26,7 @@ export class Lightbox {
     }
     /**
      * Close lightbox
-     * @param {MouseEvent} e 
+     * @param {MouseEvent/KeyboardEvent} e 
      */
     close (e) {
         e.preventDefault();
@@ -38,21 +39,77 @@ export class Lightbox {
 
     /**
      * 
+     * @param {MouseEvent/KeyboardEvent} e 
+     */
+     next (e) {
+        e.preventDefault();
+        const i = Object.keys(this._tab).forEach(key => {
+           // console.log(typeof(this._tab[key]))
+             console.log(this._tab[key]._id === parseInt(this._id))
+        });
+       
+    }
+
+    /**
+     *
+     * @param {MouseEvent/KeyboardEvent} e 
+     */
+     previous (e) {
+        e.preventDefault();
+       
+    }
+
+    /**
+     * 
      * @param {string} url URL de l'image
      * @return {HTMLElement}
      */
-    buildDom (url) {
+    buildDom (url, id) {
         const dom = document.createElement('div');
         dom.className = 'lightbox';
-        dom.innerHTML = `<button class="lightbox__close">Fermer</button>
-        <button class="lightbox__next">Suivant</button>
-        <button class="lightbox__previous">précédent</button>
-        <div class="lightbox__container">
-            <img src="${url}" alt="">
-        </div>`;
-        dom.querySelector('.lightbox__close').addEventListener('click', this.close.bind(this))
-        return dom;
 
+            const buttonClose = document.createElement('button');
+            buttonClose.className = "lightbox__close";
+
+            const buttonNext = document.createElement('button');
+            buttonNext.className = "lightbox__next";
+
+            const buttonPrevious = document.createElement('button');
+            buttonPrevious.className = "lightbox__previous";
+
+            const container = document.createElement('div');
+            container.className = "lightbox__container";
+
+                if(/\.(jpg|gif|png)$/.test(url) ) {
+                    const image = document.createElement('img');
+                    image.src = url;
+                    image.id = id;
+                    image.alt = "";
+                    image.style.height = "90vh";
+                    container.appendChild(image);
+                }
+                if((/\.(mp4)$/.test(url) )) {
+                    const video = document.createElement('video');
+                    video.style.height = "90vh";
+                    video.autoplay = true;
+                        const source = document.createElement('source');
+                        source.src = url;
+                        source.id = id;
+                        source.type = "video/mp4";
+                        video.appendChild(source);
+                    container.appendChild(video);
+                }
+            dom.appendChild(buttonClose);
+            dom.appendChild(buttonNext);
+            dom.appendChild(buttonPrevious);
+            dom.appendChild(container);
+
+        dom.querySelector('.lightbox__close').addEventListener('click', this.close.bind(this))
+
+        dom.querySelector('.lightbox__next').addEventListener('click', this.next.bind(this))
+
+        dom.querySelector('.lightbox__previous').addEventListener('click', this.previous.bind(this))
+        return dom;
     }
 }
 
