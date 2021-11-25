312aa04d7ad3532604201dd03e02836a7ea4b762
LauNM <laurie.beduneau@gmail.com>
2021-11-25 09:17:45 +0100

    test not working for github page

--- js/data.js
+++ js/data.js
@@ -1,12 +1,12 @@
 export const getPhotographersData = async () =>
-    fetch('/data.json').then(response => {
+    fetch('../data.json').then(response => {
         return response.json();
     }).then(({ photographers }) => {
         return ({ photographers })
     })
 
 export const getMediaData = async () =>
-    fetch('/data.json').then(response => {
+    fetch('../data.json').then(response => {
         return response.json();
     }).then(({ media }) => {
         return ({ media })
